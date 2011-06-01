1d3550a3c58831c636ee6bf231bafec5a039022c
pdaian <phil@linux.com>
2011-06-01 07:39:24 -0700

    added threading for playerJoin

--- src/com/mcbouncer/plugin/MCBPlayerListener.java
+++ src/com/mcbouncer/plugin/MCBPlayerListener.java
@@ -1,6 +1,8 @@
 package com.mcbouncer.plugin;
 
 import com.mcbouncer.util.MCBouncerUtil;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -13,12 +15,31 @@ public class MCBPlayerListener extends PlayerListener {
     }
     
     @Override
-    public void onPlayerJoin(PlayerJoinEvent event) { // Thread this
-        if (MCBouncerUtil.isAllowedToJoin(event.getPlayer().getName(), event.getPlayer().getAddress().toString())) { // Not sure
-            event.getPlayer().kickPlayer(MCBouncerUtil.getBanReason(event.getPlayer().getName()));
-            return;
-        }
-        MCBouncerUtil.modMessage();
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Runnable r = new Runnable() {
+            String playerName, IP;
+            Player player;
+            public void Runnable(Player player) {
+                this.player = player;
+            }
+
+            public void run() {
+                this.playerName = player.getName();
+                this.IP = player.getAddress().toString();
+                if (MCBouncerUtil.isAllowedToJoin(playerName, IP)) {
+                    player.kickPlayer(MCBouncerUtil.getBanReason(playerName));
+                    return;
+                }
+                int numBans = MCBouncerUtil.getBanCount(playerName, IP);
+                int numNotes = MCBouncerUtil.getNoteCount(playerName);
+                String response = (numBans > 0) ? numBans + " bans." : "";
+                response += numNotes > 0 ? " " + numNotes + " notes." : "";
+                if (!response.isEmpty()) {
+                    MCBouncerUtil.modMessage(ChatColor.GREEN + response);
+                }
+            }
+        };
+        r.run();
     }
     
 }
--- src/com/mcbouncer/util/MCBouncerUtil.java
+++ src/com/mcbouncer/util/MCBouncerUtil.java
@@ -2,8 +2,14 @@ package com.mcbouncer.util;
 
 public class MCBouncerUtil {
 
-    public static void addBan(String playerName) {
-        
+    public static void addBan(String playerName, String admin, String reason) {
+        MCBouncerAPI.addBan(playerName, MCBouncerConfig.getApiKey(), admin, reason);
+    }
+    public static int getBanCount(String playerName, String IP) {
+        return (int) (MCBouncerAPI.getBanCount(playerName, MCBouncerConfig.getApiKey()) + MCBouncerAPI.getIPBanCount(IP, MCBouncerConfig.getApiKey()));
+    }
+    public static int getNoteCount(String playerName) {
+        return (int) MCBouncerAPI.getNoteCount(playerName, MCBouncerConfig.getApiKey());
     }
     public static boolean isAllowedToJoin(String playerName, String IP) {
         MCBouncerAPI.updateUser(playerName, MCBouncerConfig.getApiKey(), IP);
