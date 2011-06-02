71523ddf036493142e44d80c3f586333b0e2c4d0
pdaian <phil@linux.com>
2011-06-02 07:47:31 -0700

    Misc. fixes

--- src/com/mcbouncer/plugin/MCBPlayerListener.java
+++ src/com/mcbouncer/plugin/MCBPlayerListener.java
@@ -5,35 +5,25 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
-import org.bukkit.event.player.PlayerQuitEvent;
 
 public class MCBPlayerListener extends PlayerListener {
-        
-    @Override
-    public void onPlayerQuit(PlayerQuitEvent event) {
-        
-    }
-    
+
     @Override
     public void onPlayerJoin(PlayerJoinEvent event) {
+        final Player player = event.getPlayer();
         Runnable r = new Runnable() {
             String playerName, IP;
-            Player player;
-            public void Runnable(Player player) {
-                this.player = player;
-            }
-
             public void run() {
                 this.playerName = player.getName();
-                this.IP = player.getAddress().toString();
+                this.IP = player.getAddress().getAddress().getHostAddress();
                 if (MCBouncerUtil.isAllowedToJoin(playerName, IP)) {
                     player.kickPlayer(MCBouncerUtil.getBanReason(playerName));
                     return;
                 }
                 int numBans = MCBouncerUtil.getBanCount(playerName, IP);
                 int numNotes = MCBouncerUtil.getNoteCount(playerName);
-                String response = (numBans > 0) ? numBans + " bans." : "";
-                response += numNotes > 0 ? " " + numNotes + " notes." : "";
+                String response = numBans > 0 ? numBans + " bans. " : " ";
+                response += numNotes > 0 ? numNotes + " notes." : "";
                 if (!response.isEmpty()) {
                     MCBouncerUtil.modMessage(ChatColor.GREEN + response);
                 }
@@ -41,5 +31,4 @@ public class MCBPlayerListener extends PlayerListener {
         };
         r.run();
     }
-    
 }
