4a78aaa8b8275624a4aef2dcc544774363e3471b
pdaian <phil@linux.com>
2011-06-20 14:32:38 -0400

    derp

--- src/main/java/com/mcbouncer/bukkit/MCBPlayerListener.java
+++ src/main/java/com/mcbouncer/bukkit/MCBPlayerListener.java
@@ -35,13 +35,13 @@ public class MCBPlayerListener extends PlayerListener {
         String IP = player.getAddress().getAddress().getHostAddress();
         MCBouncerUtil.updateUser(playerName, IP);
         if (MCBouncerUtil.isBanned(playerName)) {
-            player.kickPlayer("Banned: " + MCBouncerUtil.getBanReason(playerName));
             this.lastKick = playerName;
+            player.kickPlayer("Banned: " + MCBouncerUtil.getBanReason(playerName));
             return;
         }
         if (MCBouncerUtil.isIPBanned(IP)) {
-            player.kickPlayer("Banned: " + MCBouncerUtil.getIPBanReason(IP));
             this.lastKick = playerName;
+            player.kickPlayer("Banned: " + MCBouncerUtil.getIPBanReason(IP));
             return;
         }
         int numBans = MCBouncerUtil.getBanCount(playerName, IP);
